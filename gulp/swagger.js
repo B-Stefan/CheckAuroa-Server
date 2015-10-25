c39455b4f242e11e9d935614ebf6120644f96e13
Stefan B <Stefan.bie@live.de>
2015-10-25 15:38:18 +0200

    - add swagger task to create typescript-client

--- gulp/swagger.js
+++ gulp/swagger.js
@@ -1,17 +1,50 @@
 import gulp from "gulp"
 import size  from "gulp-size"
 import fs from "fs"
-
-
+import request from "request"
+import download from "gulp-download";
+import unzip from "unzip";
 gulp.task("swagger", (callback)=>{
 
     var CodeGen = require('swagger-js-codegen').CodeGen;
 
     var file = 'api/swagger.json';
-    var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
-    var nodejsSourceCode = CodeGen.getNodeCode({ className: 'Test', swagger: swagger });
 
-    console.log(nodejsSourceCode);
-    var output = fs.writeFileSync("api/api.js", nodejsSourceCode);
-    callback();
+
+    var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
+/*
+    var nodejsSourceCode = CodeGen.getCustomCode({
+        className: 'CheckAuroraClient',
+        swagger: swagger,
+        template: {
+            class: fs.readFileSync(__dirname + '/templates/node-class.mustache', 'utf-8'),
+            method: fs.readFileSync(__dirname + '/templates/node-method.mustache', 'utf-8'),
+            request: fs.readFileSync(__dirname + '/templates/node-request.mustache', 'utf-8')
+        }
+
+    });
+    */
+
+    var data = {
+        "spec": swagger
+    }
+
+    request.post({
+        url: "http://generator.swagger.io/api/gen/clients/typescript-node",
+        json:true,
+        body:data
+    },(err, req,body)=>{
+
+        console.log(err,body)
+
+        let path = __dirname + '/../api/clients/';
+        request(body.link)
+            .pipe(unzip.Extract({ path: path})).on("end",function(){
+            callback();
+        })
+
+
+
+    });
+
 });
