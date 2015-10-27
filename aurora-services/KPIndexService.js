2529dc8abcb5f102a08303f29d4c02575670c025
Stefan B <Stefan.bie@live.de>
2015-10-27 19:55:43 +0200

    - add 3 day prediction api

--- aurora-services/KPIndexService.js
+++ aurora-services/KPIndexService.js
@@ -3,21 +3,39 @@ import request from "request"
 import {isDevMode,unixToRFC3339Date} from "./../utils"
 import moment from "moment";
 import KpWingService from "./KpServices/KpWingService"
+import Kp3DayForecast from "./KpServices/Kp3DayForecast"
 
 export default class KPIndexService {
 
     constructor(){
         this.kpWingService =  new KpWingService();
+        this.kp3DayService =  new Kp3DayForecast();
 
     }
 
+    getKpList(){
+
+        return new Promise((resolve,reject)=>{
+            let kp3Days = this.kp3DayService.getKpIndexForNextDays();
+            let kpWing = this.kpWingService.getList();
+
+            console.log(kpWing,kp3Days)
+            Promise.all([kpWing,kp3Days]).then((data)=>{
+
+                let flattern = [].concat.apply([],data);
+                console.log("RESOLVE",JSON.stringify(flattern,null,4));
+                resolve(flattern);
+
+            }).catch(reject)
+        })
+    }
     getKpListByUTCDate(UTCDate){
         let unixUtcTime = UTCDate;
 
         return new Promise((resolve,reject)=>{
 
             //Get the list
-            this.kpWingService.getList().then((list)=>{
+            this.getKpList().then((list)=>{
                 let nextKpInformation =
                     list.filter((kpInformation)=> {
                             return (kpInformation.utc - unixUtcTime) > 0
@@ -40,7 +58,7 @@ export default class KPIndexService {
 
             let unixUtcTime = UTCDate;
             //Get the list
-            this.kpWingService.getList().then((list)=>{
+            this.getKpList().then((list)=>{
                 let nextKpInformation =
                     list.map((kpInformation)=> {
                             return {
---
+++ aurora-services/KpServices/Kp3DayForecast.js
@@ -0,0 +1,130 @@
+import request from "request"
+import KpInformationExtened  from "./../../aurora-classes/KpInformationExtended"
+import {unixToRFC3339Date}  from "./../../utils"
+import moment from "moment"
+export default class Kp3DayForecast{
+
+    static get URL(){return "http://services.swpc.noaa.gov/text/3-day-forecast.txt"}
+
+    static get wantedRows(){
+        return [
+            "00-03UT",
+            "03-06UT",
+            "06-09UT",
+            "09-12UT",
+            "12-15UT",
+            "15-18UT",
+            "18-21UT",
+            "21-00UT"
+        ]
+
+
+    }
+
+    parseRawData(rawData){
+        let currentDate = moment.utc();
+        currentDate.hours(0);
+        currentDate.minutes(0);
+        currentDate.seconds(0);
+
+        /**
+         * Example data:
+         *
+         *
+         *
+         * :Product: 3-Day Forecast
+         :Issued: 2015 Oct 27 1230 UTC
+         # Prepared by the U.S. Dept. of Commerce, NOAA, Space Weather Prediction Center
+         #
+         A. NOAA Geomagnetic Activity Observation and Forecast
+
+         The greatest observed 3 hr Kp over the past 24 hours was 1 (below NOAA
+         Scale levels).
+         The greatest expected 3 hr Kp for Oct 27-Oct 29 2015 is 4 (below NOAA
+         Scale levels).
+
+         NOAA Kp index breakdown Oct 27-Oct 29 2015
+
+         Oct 27     Oct 28     Oct 29
+         00-03UT        0          3          3
+         03-06UT        1          2          2
+         06-09UT        1          2          2
+         09-12UT        1          2          3
+         12-15UT        1          2          3
+         15-18UT        1          2          3
+         18-21UT        1          2          3
+         21-00UT        2          2          4
+
+         Rationale: No G1 (Minor) or greater geomagnetic storms are expected.  No
+         significant transient or recurrent solar wind features are forecast.
+
+
+         * @type {Array}
+         */
+
+        let kpInformations = rawData.split("\n") // split to get an array
+                                    .filter((line)=>{
+                                        /**
+                                         * Filter the line that match the wantedRows array
+                                         *
+                                         */
+                                        let firstLetters = line.substr(0,Kp3DayForecast.wantedRows[0].length);
+                                        return Kp3DayForecast.wantedRows.indexOf(firstLetters) > 0;
+                                    }).map((row,i)=>{
+                                            //Now we have only the rows that we want with the following format:
+                                            /**
+                                             *  00-03UT        0          3          3
+                                             * @type {number}
+                                             */
+
+
+                                            //get the hours and form from there an date
+                                            let hours  = parseFloat(row.substr(0,2)) + 1.5;
+                                            let currentDateHours = moment(currentDate).add(hours, "hours");
+
+                                            //Split by spaces, filter all empty entries, and remove the 00-03UT values out of the arr
+                                            //After this steps we have an array like [0,3,3]
+                                            let data = row  .split(" ")
+                                                            .filter((item)=>item.length != 0)
+                                                            .slice(1)
+                                                            .map((kpValue,index)=>{
+
+                                                                //Now we can map all kp values in the array to an KPInformation instance
+
+
+                                                                let kpInformation = new KpInformationExtened();
+
+                                                                //get the utc by using the pre calculated currentDateHours and add there the days
+                                                                kpInformation.utc = moment(currentDateHours).add(index,"days").unix();
+                                                                kpInformation.date = unixToRFC3339Date(kpInformation.utc);
+                                                                kpInformation.original = row;
+                                                                kpInformation.kpValue = parseInt(kpValue);
+                                                                return kpInformation;
+                                                            });
+                                            return data;
+
+                                    });
+
+        return [].concat.apply([],kpInformations); //Flattern array so make  [[ {}, {}],[{},{}]] => [{},{},{},{}]
+    }
+
+
+    getKpIndexForNextDays(){
+
+        return new Promise((resolve,reject)=>{
+
+            request(Kp3DayForecast.URL,(err,request,body)=>{
+                if(err){
+                    reject(err);
+                    return;
+                }
+                let kpInformations = this.parseRawData(body);
+                resolve(kpInformations)
+
+            })
+
+        });
+    }
+
+
+}
