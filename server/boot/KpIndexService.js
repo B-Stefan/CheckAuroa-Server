2feb1ef222b96c7ac78abaf267733b6125f8febd
Stefan B <Stefan.bie@live.de>
2016-03-20 14:42:55 +0100

    - add boot script for continuously crawling the noaa service

---
+++ server/boot/KpIndexService.js
@@ -0,0 +1,26 @@
+import KPIndexDatabaseImporter from "./../aurora-services/KPIndexDatabaseImporter"
+
+
+module.exports = function (server, callback) {
+  console.log("boot");
+
+  let databaseImporter = new KPIndexDatabaseImporter(server);
+
+  setInterval(()=>{
+    databaseImporter.startNewImport().then((results)=>{
+      console.log("new results are there ")
+    }).catch((err)=>{
+      throw new Error(err)
+    });
+  },60 * 1000);
+
+
+  databaseImporter.startNewImport().then((results)=>{
+    console.log("Init results");
+    callback();
+  }).catch((err)=>{
+    throw new Error(err)
+  });
+
+
+};
