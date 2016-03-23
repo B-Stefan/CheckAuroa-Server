5e631e79b3303ba7e967bfdaa4e6a16049e8c944
Stefan B <Stefan.bie@live.de>
2016-03-23 16:56:26 +0100

    - add test for kpIndex/current route - set timeout for test to 5 sec

--- test/KPIndex.test.js
+++ test/KPIndex.test.js
@@ -3,17 +3,33 @@ var assert = require('assert');
 var app = require('../server/server.js'); //path to app.js or server.js
 
 
+var KpIndex;
+
+var TestDataBuilder = require('loopback-testing').TestDataBuilder;
+var ref = TestDataBuilder.ref;
+
 // The context object to hold the created models.
 // You can use `this` in mocha test instead.
 var context = {};
-var KpIndex;
+
+var ref = TestDataBuilder.ref;
+
 
 
 /**
- * Tests for KPIndex route 
+ * Tests for KPIndex route
  */
 describe('/KpIndex', function() {
+  this.timeout(5000);
   lt.beforeEach.withApp(app);
+
+  /**
+   * Wait for app to get started
+   * @Todo modify to real async processing instead of only wait and hope.
+   */
+  beforeEach(function(callback){
+    setTimeout(callback,1000)
+  });
   lt.describe.whenCalledRemotely('GET', '/api/KpIndices', function () {
 
     lt.it.shouldBeAllowed();
@@ -25,7 +41,6 @@ describe('/KpIndex', function() {
       date: new Date()
     });
     it('should respond with an array of KpIndices', function () {
-      console.log(this.res.body);
       assert(Array.isArray(this.res.body));
 
     });
@@ -39,10 +54,26 @@ describe('/KpIndex', function() {
     });
 
     it('should respond with an number ', function () {
-      console.log(this.res.body);
       assert.notEqual(this.res.body.count, undefined);
-      assert.equal(this.res.body.count, 0);
+      assert.ok(this.res.body.count > 0);
 
+
+    });
+  });
+
+  lt.describe.whenCalledRemotely('GET', '/api/KpIndices/current', function () {
+
+    lt.it.shouldBeAllowed();
+    it('should have statusCode 200', function() {
+      assert.equal(this.res.statusCode, 200);
+    });
+
+    it('should respond with one KPIndex entry ', function () {
+
+      assert(this.res.body instanceof  Object);
+      assert(this.res.body.hasOwnProperty("date"));
+      assert(this.res.body.hasOwnProperty("utc"));
+      assert(this.res.body.hasOwnProperty("kpValue"));
 
     });
   });
