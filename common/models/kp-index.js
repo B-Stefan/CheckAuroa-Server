7655ce2ace453e4475abff78105eb81cd2d42575
Stefan B <Stefan.bie@live.de>
2016-03-23 17:27:57 +0100

    - add route for future only items

--- common/models/kp-index.js
+++ common/models/kp-index.js
@@ -1,17 +1,16 @@
-
 import moment from "moment"
-import {findNextKPIndexForUTC,unixToRFC3339Date} from "./../../server/utils"
-module.exports = function(KpIndex) {
+import {findNextKPIndexForUTC, unixToRFC3339Date} from "./../../server/utils"
+module.exports = function (KpIndex) {
   /*
-  KpIndex.observe('access', function(ctx, next) {
-    next()
+   KpIndex.observe('access', function(ctx, next) {
+   next()
 
-  });
-  KpIndex.sharedClass.methods().forEach((method)=>{
+   });
+   KpIndex.sharedClass.methods().forEach((method)=>{
 
-    console.log(method.name, method.isStatic);
+   console.log(method.name, method.isStatic);
 
-  });
+   });
    */
 
 
@@ -26,18 +25,18 @@ module.exports = function(KpIndex) {
   KpIndex.disableRemoteMethod("createChangeStream", true);
   KpIndex.disableRemoteMethod("upsert", true);
 
-  KpIndex.current = function(cb) {
+  KpIndex.current = function (cb) {
     let currentDate = moment().utc().utcOffset(0);
 
-    let min = currentDate.add(-1,"days").unix();
-    let max = currentDate.add(1,"days").unix();
+    let min = currentDate.add(-1, "days").unix();
+    let max = currentDate.add(1, "days").unix();
 
     KpIndex.find({
       where: {
-        utc: {between: [min,max]}
+        utc: {between: [min, max]}
       }
-    }, (err, results)=>{
-      let result = findNextKPIndexForUTC(currentDate.unix(),results);
+    }, (err, results)=> {
+      let result = findNextKPIndexForUTC(currentDate.unix(), results);
       cb(err, result);
     });
   };
@@ -48,5 +47,28 @@ module.exports = function(KpIndex) {
         returns: {arg: 'current', type: "KpIndex", root: true}
       }
   );
+
+
+  KpIndex.prediction = function (cb) {
+    let currentDate = moment().utc().utcOffset(0).unix();
+
+    KpIndex.find({
+      where: {
+        utc: {gt: currentDate}
+      }
+    }, cb);
+  };
+  KpIndex.remoteMethod(
+      'prediction',
+      {
+        http: {path: '/prediction', verb: 'get'},
+        returns: {
+          arg: 'items',
+          root: true,
+          type: ["KpIndex"]
+        }
+      }
+  );
+
 
 };
