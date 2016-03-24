0fe96f5e186098306d1f4cf47ce5f6154e233774
Stefan B <Stefan.bie@live.de>
2016-03-24 10:48:49 +0100

    - add logginf for error - set interval to 5 minutes

--- server/boot/AuroraNowcastCrawler.js
+++ server/boot/AuroraNowcastCrawler.js
@@ -10,14 +10,17 @@ module.exports = function (server) {
     console.time("nowcast");
     nowCastAurora.getList().then((result)=>{
       console.timeEnd("nowcast");
-      server.models.AuroraNowcast.upsert(result);
+      server.models.AuroraNowcast.upsert(result, (err, item)=>{
+          if(err) console.error(err);
+          else console.log("yeeah", item.getId())
+      });
       console.log("new prob results are there ", result.validAt.format());
     }).catch((err)=>{
       throw new Error(err)
     });
   }
 
-  setInterval(crawl,50000);
+  setInterval(crawl,1000 * 60 * 5);
   crawl();//init
 
 
