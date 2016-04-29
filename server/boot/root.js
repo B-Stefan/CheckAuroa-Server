496a4e771978edd15cbc914f561689fe6962567d
Stefan B <Stefan.bie@live.de>
2016-04-29 17:48:10 +0200

    - add load test verify token

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,5 +2,8 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/loaderio-2b88164b71707f9d69ea0a4fc9112222/', (req,res)=>{
+    res.send("loaderio-2b88164b71707f9d69ea0a4fc9112222")
+  });
   server.use(router);
 };
