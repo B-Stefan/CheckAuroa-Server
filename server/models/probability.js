f758dd7b2c29f2dd343753c62df0822ab4caa43e
Stefan B <Stefan.bie@live.de>
2016-10-15 19:30:24 +0200

    - add cached service for prediction

--- server/models/probability.js
+++ server/models/probability.js
@@ -1,13 +1,11 @@
 import moment from "moment"
 import NowCastAuroraService from "./../services/aurora-services/NowCastAuroraService"
-import PredictionService from "./../services/PredictionService"
+import PredictionServiceDatabase from "./../services/PredictionServiceDatabase"
 import SuncalcService from "./../services/SuncalcService"
  
 module.exports = function(Probability) {
 
-  const predictionService = new PredictionService();
-
-  const sunCalcService = new SuncalcService();
+  
 
   Probability.disableRemoteMethod("exists", true);
   Probability.disableRemoteMethod("deleteById", true);
@@ -22,9 +20,12 @@ module.exports = function(Probability) {
   
 
   Probability.prediction= function (date,lat,lng,cb) {
+    const predictionService = new PredictionServiceDatabase(Probability.app);
+
+    const sunCalcService = new SuncalcService();
 
     //Get prediction and warp result with the location 
-    return Promise.all(predictionService.get24HourPrediction(date,lat,lng,Probability.app.models.KpIndex)).then((results)=>{
+    return Promise.all(predictionService.get24HourPrediction(date,lat,lng,null)).then((results)=>{
       let list = results.map((item)=>{
         item.location = {lat: lat, lng:lng};
         return item;
