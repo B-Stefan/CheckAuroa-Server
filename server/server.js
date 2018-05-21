b19794ae27642428354d1c901431fe1bbf5f4198
Stefan Bieliauskas <sbieliauskas@rightmart.de>
2018-05-21 11:15:55 +0200

    Remove pmx and transform to promise based bootstrap

--- server/server.js
+++ server/server.js
@@ -1,43 +1,47 @@
 require("babel-register")();
-require("pmx").init({
-  http          : true, // HTTP routes logging (default: false)
-  http_latency  : 200,  // Limit of acceptable latency
-  http_code     : 500,  // Error code to track'
-  alert_enabled : true,  // Enable alerts (If you add alert subfield in custom it's going to be enabled)
-  ignore_routes : [/socket\.io/, /notFound/], // Ignore http routes with this pattern (default: [])
-  errors        : true, // Exceptions loggin (default: true)
-  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics (default: true)
-  network       : true, // Network monitoring at the application level (default: false)
-  ports         : true  // Shows which ports your app is listening on (default: false)
-});
-var loopback = require('loopback');
-var boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+function bootstrap (){
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
+  return new Promise((resolve, reject)=>{
+    const loopback = require('loopback');
+    const boot = require('loopback-boot');
+
+    const app = module.exports = loopback();
+
+    app.start = function() {
+      // start the web server
+      const server =  app.listen(function() {
+        app.emit('started');
+        const baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+          const explorerPath = app.get('loopback-component-explorer').mountPath;
+          console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+        resolve({app, server});
+      });
+    };
+
+    // Bootstrap the application, configure models, datasources and middleware.
+    // Sub-apps like REST API are mounted via boot scripts.
+    boot(app, {
+      appRootDir: __dirname,
+      bootDirs: ["./boot-server"]
+    }, function(err) {
+      if(err) reject(err);
+
+      app.start();
+
+    });
+
   });
-};
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, {
-  appRootDir: __dirname,
-  bootDirs: ["./boot-server"]
-}, function(err) {
-  if(err) console.error(err);
+}
 
-  // start the server if `$ node server.js`
-  if (require.main === module && process.env["AURORA_MODE"] != "crawler"){
-    app.start();
-  }
-});
+if (require.main === module) {
+  bootstrap().catch((err)=>{
+    console.error(err)
+  })
+} else {
+  module.exports = bootstrap;
+}
